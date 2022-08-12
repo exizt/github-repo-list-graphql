f63d5e761536a87bda397c1055e0722415972536
exizt <e2xist@gmail.com>
2022-08-12 21:36:20 +0900

    optioneventbind 개선.

--- dist/option-event-bind.js
+++ dist/option-event-bind.js
@@ -1,44 +1,42 @@
 export class OptionEventBind {
     selectors = {};
-    options = {};
     constructor(selectors = {}) {
         this.selectors = selectors;
     }
     bindEventAll(e) {
         Object.entries(this.selectors).forEach(([key, value]) => {
-            this.bindEvent(key, value, e);
+            const selector = value;
+            const elements = document.querySelectorAll(selector);
+            elements.forEach(el => {
+                this.bindEvent(key, el, e);
+            });
         });
     }
-    bindEvent(key, selector, e) {
-        const el = document.querySelector(selector);
+    bindEvent(key, el, e) {
         if (el === null)
             return;
         switch (el.tagName.toLowerCase()) {
             case 'input':
                 const type = el.type;
                 if (type == 'text' || type == 'search') {
-                    this._add_event(el, 'input', e);
+                    this._add_event_el(el, 'input', e);
+                }
+                else if (type == 'hidden') {
+                    break;
                 }
                 else {
-                    this._add_event(el, 'change', e);
+                    this._add_event_el(el, 'change', e);
                 }
                 break;
             case 'a':
-                this._add_event(el, 'click', (event) => {
-                    this.options[key] = true;
-                    e(event);
-                });
+                this._add_event_el(el, 'click', e);
                 break;
             default:
-                this._add_event(el, 'change', e);
+                this._add_event_el(el, 'change', e);
                 break;
         }
     }
-    _add_event(el, type, event) {
-        this.options = {};
+    _add_event_el(el, type, event) {
         el?.addEventListener(type, event);
-    }
-    get_option(key) {
-        return this.options[key];
     }
 }
--- src/option-event-bind.ts
+++ src/option-event-bind.ts
@@ -1,6 +1,5 @@
 export class OptionEventBind {
     selectors = {}
-    options = {}
 
     constructor(selectors = {}){
         this.selectors = selectors
@@ -8,12 +7,17 @@ export class OptionEventBind {
 
     bindEventAll(e:EventListener){
         Object.entries(this.selectors).forEach(([key, value]) => {
-            this.bindEvent(key, (value as string), e)
+            // this.bindEvent(key, (value as string), e)
+            const selector = (value as string)
+            const elements = document.querySelectorAll(selector)
+            elements.forEach(el => {
+                this.bindEvent(key, el as HTMLElement, e)
+            })
         })
     }
 
-    private bindEvent(key:string, selector:string, e:EventListener){
-        const el = document.querySelector(selector) as HTMLElement;
+    private bindEvent(key:string, el:HTMLElement, e:EventListener){
+        // const el = document.querySelector(selector) as HTMLElement;
         if (el === null) return;
 
         switch (el.tagName.toLowerCase()) {
@@ -21,32 +25,28 @@ export class OptionEventBind {
                 const type = (el as HTMLInputElement).type
                 if(type == 'text' || type == 'search'){
                     // 문자열 입력시 발생되는 이벤트
-                    this._add_event(el, 'input', e)    
+                    this._add_event_el(el, 'input', e)
+                } else if(type=='hidden') {
+                    // 참고. 기본적으로 hidden 타입은 change 이벤트가 발생하지 않는다고 함.
+                    // 그래도 혹시 모르므로, hidden 타입인 경우에는 이벤트 발생시키지 않도록 함.
+                    // https://stackoverflow.com/a/8965804
+                    break
                 } else {
                     // 값이 변경된 후 포커스가 벗어날 때 발생되는 이벤트
-                    this._add_event(el, 'change', e)
+                    this._add_event_el(el, 'change', e)
                 }
                 break
             case 'a':
-                this._add_event(el, 'click', (event) => {
-                    this.options[key] = true
-                    e(event)
-                })
+                this._add_event_el(el, 'click', e)
                 break
             default:
-                this._add_event(el, 'change', e)
+                this._add_event_el(el, 'change', e)
                 break
         }
     }
 
-    private _add_event(el:HTMLElement, type:string, event:EventListener){
-        // 옵션 초기화
-        this.options = {}
+    private _add_event_el(el:HTMLElement, type:string, event:EventListener){
         el?.addEventListener(type, event);
-    }
-
-    get_option(key:string){
-        return this.options[key]
     }
 }
 
